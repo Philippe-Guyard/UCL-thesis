84e718c49743541697fdf7337c6c246fe462ab88
Philippe Guyard <philippe.guyard@outlook.com>
2024-06-26 13:27:55 +0200

    Allow gemma pretraining

--- lib_clean/models.py
+++ lib_clean/models.py
@@ -7,7 +7,10 @@ from transformers import (
     Qwen2ForCausalLM,
     PreTrainedTokenizer,
     LlamaConfig,
-    LlamaForCausalLM
+    LlamaForCausalLM,
+    GemmaConfig,
+    AutoModelForCausalLM,
+    GemmaForCausalLM
 )
 
 from accelerate import PartialState
@@ -19,7 +22,7 @@ def get_opt(model_name: str):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return model, tokenizer
 
-def get_qwen2(model_name: str, with_relu=False):
+def get_gated_model(model_name: str, with_relu=False):
     config = None 
     if with_relu:
         config = AutoConfig.from_pretrained(model_name)
@@ -28,30 +31,17 @@ def get_qwen2(model_name: str, with_relu=False):
 
     device_string = PartialState().process_index
 
-    model = Qwen2ForCausalLM.from_pretrained(model_name, config=config, device_map={'': device_string})
+    model = AutoModelForCausalLM.from_pretrained(model_name, config=config, device_map={'': device_string})
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return model, tokenizer
 
-def get_llama(model_name: str, with_relu=False):
-    config = None 
-    if with_relu:
-        config = AutoConfig.from_pretrained(model_name)
-        config: LlamaConfig 
-        config.hidden_act = 'relu'
-
-    device_string = PartialState().process_index
-
-    model = LlamaForCausalLM.from_pretrained(model_name, config=config, device_map={'': device_string})
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    return model, tokenizer
-
-
-ModelType = OPTForCausalLM | Qwen2ForCausalLM
+ModelType = OPTForCausalLM | Qwen2ForCausalLM | GemmaForCausalLM | LlamaForCausalLM
 def get_model(model_name: str, **model_kwargs) -> Tuple[ModelType, PreTrainedTokenizer]:
     getters_map = {
-        'qwen2': get_qwen2,
+        'qwen2': get_gated_model,
         'opt': get_opt,
-        'llama': get_llama,
+        'llama': get_gated_model,
+        'gemma': get_gated_model
     } 
 
     for key, func in getters_map.items():
