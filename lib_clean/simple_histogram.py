8f2c74700b4d6216eaf16c55ccea7b2824ac30ec
Philippe Guyard <philippe.guyard@outlook.com>
2024-08-24 06:17:51 +0100

    Save histograms when training assistants

---
+++ lib_clean/simple_histogram.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class SimpleHistogram:
+    def __init__(self, precision=0.001, min_range=-1, max_range=1):
+        self.precision = precision
+        self.min_range = min_range
+        self.max_range = max_range
+        self.bin_count = int((max_range - min_range) / precision)
+        self.histogram = np.zeros(self.bin_count)
+        self._write_mode = True
+        self.cumsum = None
+
+    def update(self, value):
+        assert self._write_mode
+        """Update histogram count based on the value."""
+        if self.min_range <= value < self.max_range:
+            bin_index = int((value - self.min_range) / self.precision)
+            self.histogram[bin_index] += 1
+    
+    def for_reading(self):
+        self._write_mode = False
+        self.cumsum = self.histogram.cumsum()
+        return self
+
+    def get_histogram(self):
+        return self.histogram 
+
+    def compute_quantile(self, value):
+        """Compute the quantile of a given number based on the histogram."""
+        if value < self.min_range:
+            return 0.0
+        elif value >= self.max_range:
+            return 1.0
+
+        bin_index = int((value - self.min_range) / self.precision)
+        
+        # Compute CDF up to the bin containing the value
+        total_counts = self.cumsum[-1]
+        cdf_value = self.cumsum[bin_index - 1] / total_counts if bin_index > 0 else 0.0
+        return cdf_value
+
+    def dump_to_file(self, filename):
+        """Save the histogram data to a file."""
+        np.save(filename, self.histogram)
+
+    def load_from_file(self, filename):
+        """Load the histogram data from a file."""
+        self.histogram = np.load(filename)
+        # Ensure the loaded histogram has the correct size
+        assert len(self.histogram) == self.bin_count, "Loaded histogram size mismatch."
